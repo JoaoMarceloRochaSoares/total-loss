db24fc534a225d081f4c406eb6e28552daa0e7f2
nickolasdutra14 <nickolas.dutra14@gmail.com>
2026-06-09 10:35:40 -0300

    Update stylesheet links in treino-para-iniciantes.php
    Signed-off-by: nickolasdutra14 <nickolas.dutra14@gmail.com>

--- html/treino-para-iniciantes.php
+++ html/treino-para-iniciantes.php
@@ -187,7 +187,8 @@ footer{
 }
 
 </style>
-    <link rel="stylesheet" href="main.css">
+    <link rel="stylesheet" href="../main.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
 </head>
 <body>
 <?php include dirname(__DIR__) . '/header.php'; ?>
