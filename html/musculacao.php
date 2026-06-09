39733eeb8eec8a0ce115ae2b564f4ffa650e731a
nickolasdutra14 <nickolas.dutra14@gmail.com>
2026-06-09 10:35:00 -0300

    Update stylesheet links in musculacao.php
    Signed-off-by: nickolasdutra14 <nickolas.dutra14@gmail.com>

--- html/musculacao.php
+++ html/musculacao.php
@@ -181,7 +181,8 @@ section{
 }
 
 </style>
-    <link rel="stylesheet" href="main.css">
+    <link rel="stylesheet" href="../main.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
 </head>
 
 <body>
