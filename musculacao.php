bcb9a3b9880cf1f7f10ff9bbceb304da662507f7
Samuelbr017 <samuelp.costa23@gmail.com>
2026-06-12 11:41:33 -0300

    Add files via upload
    Signed-off-by: Samuelbr017 <samuelp.costa23@gmail.com>

--- musculacao.php
+++ musculacao.php
@@ -203,7 +203,7 @@ $usuario_nome = $_SESSION['usuario_nome'] ?? null;
     <div id="menu-btn" class="fas fa-bars"></div>
 
     <nav class="navbar">
-        <a href="/index.php">Início</a>
+        <<a href="index.php">Início</a>
         <a href="/index.php#about">Sobre</a>
         <a href="/index.php#features">Serviços</a>
         <a href="/index.php#suppl">Suplementos</a>
@@ -211,17 +211,17 @@ $usuario_nome = $_SESSION['usuario_nome'] ?? null;
         <a href="/index.php#support">Suporte</a>
 
         <?php if ($usuario_nome): ?>
-            <a href="/conta.php" style="color:#fff">
+            <a href="conta.php" style="color:#fff">
                 <i class="fas fa-user-circle" style="margin-right:.4rem"></i>
                 <?= htmlspecialchars($usuario_nome) ?>
             </a>
         <?php else: ?>
-            <a href="/Login/login.php">Login</a>
+            <a href="Login/login.php">Login</a>
         <?php endif; ?>
     </nav>
 </header>
 
-<section class="hero-musculacao">
+<section class="hero-musculacao musculacao-page">
 
     <div class="hero-content">
         <span>Treino de Força</span>
