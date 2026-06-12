75677948e8b27f59b3d529d47b31400f2805ad34
Samuelbr017 <samuelp.costa23@gmail.com>
2026-06-11 23:54:07 -0300

    Add files via upload
    Signed-off-by: Samuelbr017 <samuelp.costa23@gmail.com>

--- treino-para-iniciantes.php
+++ treino-para-iniciantes.php
@@ -80,6 +80,7 @@ body{
     transform:translateY(-3px);
 }
 
+
 .section-title{
     text-align:center;
     margin:80px 0 50px;
@@ -95,6 +96,7 @@ body{
     margin-top:10px;
 }
 
+
 .cards-grid{
     width:90%;
     max-width:1400px;
@@ -105,6 +107,7 @@ body{
     gap:30px;
     margin-bottom:35px;
 }
+
 
 .card{
     background:#0d0d0d;
@@ -142,6 +145,7 @@ body{
 .card li:last-child{
     border:none;
 }
+
 
 .info{
     width:90%;
@@ -207,8 +211,7 @@ footer{
         </p>
 
         <a href="#treinos" class="btn">Ver Treinos</a>
-
-<a href="index.php#mar" class="btn" style="margin-left:10px;">
+        <a href="index.php#mar" class="btn" style="margin-left:10px;">
     Voltar às Matérias
 </a>
 
@@ -222,6 +225,8 @@ footer{
     <h2>Escolha Seu Treino</h2>
 
 </div>
+
+
 <div class="cards-grid">
 
     <div class="card">
@@ -231,19 +236,197 @@ footer{
         <div class="card-content">
 
             <h3>Corpo Inteiro</h3>
-            </div>
+
+            <ul>
+                <li>Agachamento 3x12</li>
+                <li>Supino Máquina 3x12</li>
+                <li>Puxada Frontal 3x12</li>
+                <li>Desenvolvimento 3x10</li>
+                <li>Prancha 3x30s</li>
+            </ul>
+
+        </div>
+
+    </div>
+
+    <div class="card">
+
+        <img src="https://totalpass.com/wp-content/uploads/2024/07/supino-reto.jpg">
+
+        <div class="card-content">
+
+
+            <h3>Superior</h3>
+
+            <ul>
+                <li>Supino Reto 3x12</li>
+                <li>Remada 3x12</li>
+                <li>Rosca Direta 3x12</li>
+                <li>Tríceps Polia 3x12</li>
+                <li>Elevação Lateral 3x15</li>
+            </ul>
+
+        </div>
+
+    </div>
+
+    <div class="card">
+
+        <img src="https://cptstatic.s3.amazonaws.com/imagens/enviadas/materias/materia29406/mulher-malhando-cpt.jpg">
+
+        <div class="card-content">
+
+
+            <h3>Inferior</h3>
+
+            <ul>
+                <li>Agachamento 3x12</li>
+                <li>Leg Press 3x15</li>
+                <li>Extensora 3x15</li>
+                <li>Flexora 3x15</li>
+                <li>Panturrilha 4x20</li>
+            </ul>
+
+        </div>
+
+    </div>
+
+</div>
+
+
+<div class="cards-grid">
+
+    <div class="card">
+
+        <img src="https://img-s-msn-com.akamaized.net/tenant/amp/entityid/AA22ZuzO.img?w=768&h=403&m=6&x=505&y=75&s=44&d=44">
+
+        <div class="card-content">
+
+            <h3>Emagrecimento</h3>
+
+            <ul>
+                <li>Polichinelo 30s</li>
+                <li>Agachamento 15 reps</li>
+                <li>Flexão 12 reps</li>
+                <li>Corrida 2 min</li>
+                <li>4 Circuitos</li>
+            </ul>
+
+        </div>
+
+    </div>
+
+    <div class="card">
+
+        <img src="https://www.fiberoficial.com.br/cdn/shop/articles/Como_Aumentar_Resistencia_Fisica_Rapidamente_Dicas_Eficazes.jpg?v=1748869032&width=1500">
+
+        <div class="card-content">
+
+
+            <h3>Resistência</h3>
+
+            <ul>
+                <li>Leg Press 20 reps</li>
+                <li>Remada 15 reps</li>
+                <li>Flexões 12 reps</li>
+                <li>Prancha 45s</li>
+                <li>4 Séries</li>
+            </ul>
+
+        </div>
+
+    </div>
+
+    <div class="card">
+
+        <img src="https://images.unsplash.com/photo-1483721310020-03333e577078?q=80&w=1200">
+
+        <div class="card-content">
+
+
+            <h3>Condicionamento</h3>
+
+            <ul>
+                <li>Esteira 10 min</li>
+                <li>Bicicleta 10 min</li>
+                <li>Abdominal 20 reps</li>
+                <li>Prancha 30s</li>
+                <li>Alongamento</li>
+            </ul>
+
+        </div>
+
+    </div>
+
+</div>
+
 
 <div class="cards-grid">
 
     <div class="card">
 
         <img src="https://generationiron.com/wp-content/uploads/2021/03/header-2-1-1024x543.jpg">
-        <div class="cards-grid">
+
+        <div class="card-content">
+
+            <h3>Calistenia Básica</h3>
+
+            <ul>
+                <li>Flexões 3x10</li>
+                <li>Agachamentos 3x20</li>
+                <li>Prancha 3x30s</li>
+                <li>Abdominal 3x20</li>
+                <li>Mobilidade</li>
+            </ul>
+
+        </div>
+
+    </div>
 
     <div class="card">
 
-        <img src="https://generationiron.com/wp-content/uploads/2021/03/header-2-1-1024x543.jpg">
-        <div class="info">
+        <img src="https://images.unsplash.com/photo-1571019613454-1cb2f99b2d8b?q=80&w=1200">
+
+        <div class="card-content">
+
+            <h3>Corpo Funcional</h3>
+
+            <ul>
+                <li>Afundo 3x12</li>
+                <li>Flexão 3x12</li>
+                <li>Prancha Lateral</li>
+                <li>Elevação Joelhos</li>
+                <li>Mobilidade</li>
+            </ul>
+
+        </div>
+
+    </div>
+
+    <div class="card">
+
+        <img src="https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEiDbVrZ7wVv3zVpi3DIiSBMtACVGqKa-G3mI4xO_fz5t1-1ZbhmXmPq_lQYd1sp8oOdVC1KALs_0Ei3HZ5yBI3AIAkN5pR6ZpGTa2-FUYpFHGKbIAQlgtku5kma_dTgpgKBGS4BMgvJChU/w1200-h630-p-k-no-nu/melhores-exercicios-triceps-flexao-diamond-push-up.jpg">
+
+        <div class="card-content">
+
+
+            <h3>Evolução Inicial</h3>
+
+            <ul>
+                <li>Flexões 4x12</li>
+                <li>Agachamentos 4x20</li>
+                <li>Barra Australiana</li>
+                <li>Prancha 45s</li>
+                <li>Corrida 5 min</li>
+            </ul>
+
+        </div>
+
+    </div>
+
+</div>
+
+<div class="info">
 
     <span>DICA PARA INICIANTES</span>
 
@@ -258,7 +441,7 @@ footer{
 
 </div>
 
-<div style="text-align:center; margin-bottom:60px;">
+<div style="text-align:center; margin-bottom:50px;">
 
     <a href="index.php#mar" class="btn">
         Voltar para Total Loss
