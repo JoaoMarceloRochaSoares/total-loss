f24d546b3cd5134d75f9656469034bcc8264dcd1
Samuelbr017 <samuelp.costa23@gmail.com>
2026-06-12 11:30:19 -0300

    Add files via upload
    Signed-off-by: Samuelbr017 <samuelp.costa23@gmail.com>

--- musculacao.php
+++ musculacao.php
@@ -198,11 +198,26 @@ $usuario_nome = $_SESSION['usuario_nome'] ?? null;
 <body>
 
 <header class="header">
-    <a href="index.php" class="logo"><span>Total</span>Loss</a>
+    <a href="/index.php" class="logo"><span>Total</span>Loss</a>
+
+    <div id="menu-btn" class="fas fa-bars"></div>
 
     <nav class="navbar">
-        <a href="index.php">Início</a>
-        <a href="produtos.php">Produtos</a>
+        <a href="/index.php">Início</a>
+        <a href="/index.php#about">Sobre</a>
+        <a href="/index.php#features">Serviços</a>
+        <a href="/index.php#suppl">Suplementos</a>
+        <a href="/index.php#mar">Matérias</a>
+        <a href="/index.php#support">Suporte</a>
+
+        <?php if ($usuario_nome): ?>
+            <a href="/conta.php" style="color:#fff">
+                <i class="fas fa-user-circle" style="margin-right:.4rem"></i>
+                <?= htmlspecialchars($usuario_nome) ?>
+            </a>
+        <?php else: ?>
+            <a href="/Login/login.php">Login</a>
+        <?php endif; ?>
     </nav>
 </header>
 
@@ -357,6 +372,8 @@ $usuario_nome = $_SESSION['usuario_nome'] ?? null;
     </div>
 
 </section>
+
+ <script src='main.js'></script>
 
 </body>
 </html>
