ab378b8fd3da8cd38937dabeb3e95d909218c642
Samuelbr017 <samuelp.costa23@gmail.com>
2026-06-11 22:52:04 -0300

    Add files via upload
    Signed-off-by: Samuelbr017 <samuelp.costa23@gmail.com>

--- index.php
+++ index.php
@@ -238,15 +238,6 @@ $usuario_nome = $_SESSION['usuario_nome'] ?? null;
             </div>
         </section>
 
-        <!-- ===== FEEDBACKS ===== -->
-        <section class="review">
-            <div class="information">
-                <span>Feedbacks</span>
-                <h3>O que nossos clientes dizem</h3>
-                <p>Quer saber o que nossos clientes falam sobre nós?</p>
-                <a href="#" class="btn">Leia Todos</a>
-            </div>
-
             <div class="swiper review-slider">
                 <div class="swiper-wrapper">
                     <div class="swiper-slide slide">
